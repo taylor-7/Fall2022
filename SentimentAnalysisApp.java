7a079f385fd71fa0b7e0b4a5b3e4bc31fef9df73
Taylor <nmi12@txstate.edu>
2022-10-25 20:56:16 -0500

    Added main

---
+++ SentimentAnalysisApp.java
@@ -0,0 +1,12 @@
+
+public class SentimentAnalysisApp{
+    public static void main (String [] args){
+        String posWords = args[0];
+        String negWords = args[1];
+        String posReviews = args[2];
+        String negReviews = args[3];
+
+        
+    }
+
+}
